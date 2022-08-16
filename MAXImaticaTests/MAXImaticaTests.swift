c3301e6adb0460417363b591ca3a7e27bb7e81de
Igor Malyarov <igor1309@gmail.com>
2022-08-16 10:26:40 +0300

    add empty unit test to `CI`

---
+++ MAXImaticaTests/MAXImaticaTests.swift
@@ -0,0 +1,15 @@
+//
+//  MAXImaticaTests.swift
+//  MAXImaticaTests
+//
+//  Created by Igor Malyarov on 16.08.2022.
+//  Copyright © 2022 Igor Malyarov. All rights reserved.
+//
+
+import XCTest
+
+final class MAXImaticaTests: XCTestCase {
+
+    func test() throws {}
+
+}
